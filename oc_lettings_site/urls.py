be4240ca2446754fc04ec8ea2502c076ce84bae5
Marwan Wafik <Marwanewafik@hotmail.fr>
2025-01-25 03:03:06 +0100

    update

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -9,13 +9,10 @@ It includes:
 """
 
 from django.contrib import admin
-from django.http import HttpResponse
 from . import views
 from django.urls import path, include
-
-def trigger_error(request):
-    division_by_zero = 1 / 0  # This will raise a ZeroDivisionError
-    return HttpResponse("This will never be reached")
+from django.conf import settings
+from django.conf.urls.static import static
 
 #: A list of URL pattern definitions for the entire project.
 urlpatterns = [
@@ -32,5 +29,4 @@ urlpatterns = [
     
     # Routes to the Django admin interface.
     path("admin/", admin.site.urls),
-    path("trigger-error/", trigger_error, name="trigger_error"),  # New path for error trigger
-]
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
--- property_lettings/urls.py
+++ property_lettings/urls.py
@@ -5,23 +5,21 @@ from . import views
 app_name = "lettings"
 
 # URL patterns for the 'lettings' app
+# - Route for the index view:
+#   Displays a list of all lettings.
+#   URL: /
+#   View: views.index
+#   Name: "index"
+#
+# - Route for the letting detail view:
+#   Displays detailed information for a specific letting.
+#   URL: /<letting_id>/
+#   View: views.letting
+#   Name: "letting"
+#   Parameters:
+#     letting_id (int): ID of the letting to be displayed.
+
 urlpatterns = [
     path("", views.index, name="index"),
-    """
-    Route for the index view.
-    Displays a list of all lettings.
-    - URL: /
-    - View: views.index
-    - Name: "index"
-    """,
     path("<int:letting_id>/", views.letting, name="letting"),
-    """
-    Route for the letting detail view.
-    Displays detailed information for a specific letting.
-    - URL: /<letting_id>/
-    - View: views.letting
-    - Name: "letting"
-    Parameters:
-        letting_id (int): ID of the letting to be displayed.
-    """,
 ]
