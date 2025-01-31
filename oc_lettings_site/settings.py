185489bf783e8f15d0c135324b54819486e5f901
Marwan Wafik <Marwanewafik@hotmail.fr>
2025-01-31 23:23:25 +0100

    Delete/Fixing remaining files

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["152.42.142.243", "localhost", "127.0.0.1", "0.0.0.0"]
 
